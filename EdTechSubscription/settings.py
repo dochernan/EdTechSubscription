70fbdee6c0bcdea998caec6b1278f08788871230
Hernan Alar <alarh@ismanila.org>
2025-04-20 09:42:12 +0800

    updated settings.py to cater static folders

--- EdTechSubscription/settings.py
+++ EdTechSubscription/settings.py
@@ -123,7 +123,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Collects static files here
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'subscriptiondb/static'),  # optional: add your app's static folder
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
